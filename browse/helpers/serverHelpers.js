de5bca76dd1b2a5d29cbf8a81ed1811629dcd1a5
Christopher Chen <chen1385@gmail.com>
2018-02-10 16:57:03 -0800

    merging serverSSH & databaseSSH

--- browse/helpers/serverHelpers.js
+++ browse/helpers/serverHelpers.js
@@ -45,7 +45,7 @@ const sendEvent = () => {
 const startBrowsing = async (userId) => {
     //when a user logs in, push the user's movie recommendations to the UI
     //a GET request to the database that results in a push to the UI;
-    let url = `http://54.191.69.185:8080/startbrowsing/${userId}`
+    let url = `http://localhost:8080/startbrowsing/${userId}`
 
     fetch(url, {
         method: 'GET',
